8fd31c39faf53fe6b084b0c7ba5ca5a144dcdc54
fudongfan <fudong@umich.edu>
2019-02-12 12:42:48 -0500

    added time stretch example script

--- bad_speed_up_voice.py
+++ bad_speed_up_voice.py
@@ -4,13 +4,12 @@ CHANNELS = 1
 swidth = 2
 Change_RATE = 10
 
-spf = wave.open('flip_a_coin.wav', 'rb')
-RATE=spf.getframerate()
-signal = spf.readframes(-1)
+with wave.open('sample_audio/flip_a_coin.wav', 'rb') as spf:
+    RATE = spf.getframerate()
+    signal = spf.readframes(-1)
 
-wf = wave.open('changed.wav', 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(swidth)
-wf.setframerate(RATE*Change_RATE)
-wf.writeframes(signal)
-wf.close()
+with wave.open('changed.wav', 'wb') as wf:
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(swidth)
+    wf.setframerate(RATE*Change_RATE)
+    wf.writeframes(signal)
