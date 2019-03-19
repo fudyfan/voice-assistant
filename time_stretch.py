9f61b137cd2ca3e7a8f7a08addcec5c43cb3f53c
fudongfan <fudong@umich.edu>
2019-03-19 12:42:53 -0400

    converted wav to correct fmat

--- time_stretch.py
+++ time_stretch.py
@@ -5,7 +5,8 @@
 import argparse
 import sys
 import librosa
-
+from pydub import AudioSegment
+import soundfile
 
 def stretch(input_file, output_file, speed):
     '''Phase-vocoder time stretch demo function.
@@ -20,10 +21,17 @@ def stretch(input_file, output_file, speed):
     '''
 
     # 1. Load the wav file, resample
-    y, sr = librosa.load(input_file)
+    y, sr = librosa.load(input_file, sr=16000, mono=True)
 
     # 2. Time-stretch through effects module
     y_stretch = librosa.effects.time_stretch(y, speed)
 
     librosa.output.write_wav(output_file, y_stretch, sr)
+
+    voice = AudioSegment.from_wav(output_file)
+    voice = voice + 10
+    voice.export(output_file, "wav")
+
+    data, samplerate = soundfile.read(output_file)
+    soundfile.write(output_file, data, samplerate, subtype='PCM_16')
 
