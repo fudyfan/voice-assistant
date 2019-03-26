741a37f00aaa9db8febd990960ea42a288044f6c
fudongfan <fudong@umich.edu>
2019-03-25 21:54:45 -0400

    removed def_input-file_string

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
+import argparse
+import sys
+import os
+import avs
+from recording import Recording
+from gpiozero import Button
+from alexa_client.alexa_client import helpers
 from low_pass_filter import apply_low_pass_filter
 from time_stretch import stretch
 from wav_convert import convert_16bit, volume_adjust
-import argparse
-import sys
-import pyaudio
-import wave
-import time
-from recording import Recording
-from gpiozero import Button
-import avs
-from alexa_client.alexa_client import helpers
-import os
-
-def_input_string = '__def_input_string__'
 
 
-def main(input_file, output_file, speed):
+def main(input_file, output_file, speed, debug=False):
     print("ready for input")
     input_file_name = input_file
     button = Button(17)
@@ -23,25 +18,25 @@ def main(input_file, output_file, speed):
     dialog_req_id = helpers.generate_unique_id()
 
     while True:
-        # record from mic?
-        if input_file == def_input_string:
+        # record from mic
+        if input_file is None:
             button.wait_for_press()
 
             input_file_name = "out.wav"
             rec = Recording(input_file_name)
             rec.record(button)
 
-        # low pass filter
-        temp_fname = "temp.wav"
-        print("Applying low-pass filter to {}".format(input_file_name))
-        apply_low_pass_filter(input_file_name, temp_fname)
+        if debug:
+            # low pass filter
+            temp_fname = "temp.wav"
+            print("Applying low-pass filter to {}".format(input_file_name))
+            apply_low_pass_filter(input_file_name, temp_fname)
 
-        # speed up
-        print("Speeding up by factor of {}".format(speed))
-        stretch(temp_fname, output_file, speed)
+            # speed up
+            print("Speeding up by factor of {}".format(speed))
+            stretch(temp_fname, output_file, speed)
 
-        volume_adjust(output_file, 15)
-        # volume_adjust(temp_fname, 15)
+            volume_adjust(output_file, 15)
 
         # make sure formatted for avs
         print("Writing to {}".format(output_file))
@@ -51,14 +46,13 @@ def main(input_file, output_file, speed):
         # send to avs
         # outfiles = avs.send_rec_to_avs(output_file, client)
         outfiles = avs.send_rec_to_avs(output_file, client, dialog_req_id)
-        # outfiles = avs.send_rec_to_avs(temp_fname, client, dialog_req_id)
 
         # play back avs response
         for of in outfiles:
             print("playing:" + of)
             os.system('omxplayer ' + of)
 
-        if input_file == def_input_string:
+        if input_file is None:
             print("Command completed! Waiting for new input!")
         else:
             break
@@ -71,7 +65,6 @@ def process_arguments(args):
 
     parser.add_argument('-i', '--input_file',
                         action='store',
-                        default=def_input_string,
                         required=False,
                         help='path to the input file (wav)')
 
@@ -87,6 +80,10 @@ def process_arguments(args):
                         default=2.0,
                         required=False,
                         help='speed')
+
+    parser.add_argument('-d', '--debug',
+                        action='store_true',
+                        help='debug mode')
 
     return vars(parser.parse_args(args))
 
