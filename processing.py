4d8d5803ab891f8dcc451fa7418b6a6e936f5ff3
fudongfan <fudong@umich.edu>
2019-03-28 11:28:11 -0400

    processing class mostly done

--- processing.py
+++ processing.py
@@ -1,52 +1,23 @@
-import argparse
 import librosa
 from pydub import AudioSegment
 import soundfile
-import numpy as np
 import wave
-import sys
 import math
 import contextlib
-import struct
 from low_pass_filter import *
 
 
 class Processing:
-    def __init__(self, input_file, output_file):
+    temp_file = 'temp.wav'
+
+    def __init__(self, input_file, output_file, speed, decibels):
         self.input_file = input_file
         self.output_file = output_file
+        self.speed = speed
+        self.decibels = decibels
 
-    def stretch(self, input_file, output_file, speed):
-        '''Phase-vocoder time stretch demo function.
-
-        :parameters:
-        - input_file : str
-            path to input audio
-        - output_file : str
-            path to save output (wav)
-        - speed : float > 0
-            speed up by this factor
-        '''
-
-        # 1. Load the wav file, resample
-        y, sr = librosa.load(input_file, sr=16000, mono=True)
-
-        # 2. Time-stretch through effects module
-        y_stretch = librosa.effects.time_stretch(y, speed)
-
-        librosa.output.write_wav(output_file, y_stretch, sr)
-
-    def convert_16bit(self, file):
-        data, samplerate = soundfile.read(file)
-        soundfile.write(file, data, samplerate, subtype='PCM_16')
-
-    def volume_adjust(self, file, decibels):
-        voice = AudioSegment.from_wav(file)
-        voice = voice + decibels
-        voice.export(file, "wav")
-
-    def apply_low_pass_filter(self, fname, outname):
-        with contextlib.closing(wave.open(fname, 'rb')) as spf:
+    def apply_low_pass_filter(self):
+        with contextlib.closing(wave.open(self.input_file, 'rb')) as spf:
             sampleRate = spf.getframerate()
             ampWidth = spf.getsampwidth()
             nChannels = spf.getnchannels()
@@ -69,8 +40,28 @@ class Processing:
             # Use moving average (only on first channel)
             filtered = running_mean(channels[0], N).astype(channels.dtype)
 
-            wav_file = wave.open(outname, "w")
+            wav_file = wave.open(self.temp_file, "w")
             wav_file.setparams((1, ampWidth, sampleRate, nFrames,
                                 spf.getcomptype(), spf.getcompname()))
             wav_file.writeframes(filtered.tobytes('C'))
             wav_file.close()
+
+    def stretch(self):
+        """Phase-vocoder time stretch function."""
+
+        # 1. Load the wav file, resample
+        y, sr = librosa.load(self.temp_file, sr=16000, mono=True)
+
+        # 2. Time-stretch through effects module
+        y_stretch = librosa.effects.time_stretch(y, self.speed)
+
+        librosa.output.write_wav(self.temp_file, y_stretch, sr)
+
+    def volume_adjust(self):
+        voice = AudioSegment.from_wav(self.temp_file)
+        voice = voice + self.decibels
+        voice.export(self.temp_file, "wav")
+
+    def convert_16bit(self):
+        data, samplerate = soundfile.read(self.temp_file)
+        soundfile.write(self.output_file, data, samplerate, subtype='PCM_16')
