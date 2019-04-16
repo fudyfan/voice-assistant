8bc5e1ef5cd8617f31e3efaf6e65d79414c75cd5
jonpenix <jonpenix@umich.edu>
2019-04-15 21:07:33 -0400

    more json changes

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def launch_menu(button, light):
                     break
 
     with open('save_state.json', 'w') as saveFile:
-        saveFile.write(json.load({"savedSpeed":SPEED}, indent=4))
+        saveFile.write(json.dumps({"savedSpeed":SPEED}, indent=4))
 
     IN_MENU = False
 
